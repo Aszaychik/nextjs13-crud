8cd1c0bedbd9828293cf54dd2d1f79fc56e86989
AsZaychik <aszaychik@gmail.com>
2022-11-06 18:25:46 +0700

    Init

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>HOME PAGE</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam voluptatibus placeat atque facere voluptates beatae delectus? Voluptatem, quisquam dolore ad cum, aut sapiente excepturi, minus molestiae error placeat consequatur accusamus.</p>
+    </div>
+  );
+}
